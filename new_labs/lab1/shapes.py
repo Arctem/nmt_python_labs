9678f1493974251d9e8e038f21a75a8b1fae599e
Arctem <rarctem@gmail.com>
2014-08-24 23:29:16 -0600

    Add new "Shapes" exercise.

---
+++ new_labs/lab1/shapes.py
@@ -0,0 +1,16 @@
+pi = 3.14
+
+radius = input('Please input the radius of the circle: ') #Get input from user.
+radius = float(radius)
+
+#Calculate and display the circumference and area of the circle.
+
+width = input('Please input the width of the box: ')
+height = input('Please input the height of the box: ')
+length = input('Please input the length of the box: ')
+
+width = float(width)
+height = float(height)
+length = float(length)
+
+#Calculate and display the surface area and volume of the box.
