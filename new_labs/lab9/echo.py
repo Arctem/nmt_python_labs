ab3ceeaf68879131736a3c354c9d658d7c6520e3
Russell White <rarctem@gmail.com>
2014-10-27 11:22:18 -0600

    First version of Lab 9: sockets. Include example of echo server/client.

---
+++ new_labs/lab9/echo.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import select
+import socket
+import sys
+
+def main():
+    host = ''
+    port = 50000
+    backlog = 5
+    size = 1024
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(backlog)
+    inputs = [server, sys.stdin]
+    running = True
+    while running:
+        input_ready, output_ready, except_ready = select.select(inputs, [], [])
+        
+        for s in input_ready:
+            if s == server:
+                client, address = server.accept()
+                print("Received connection from {}.".format(address))
+                inputs.append(client)
+            elif s == sys.stdin:
+                #handle standard input
+                junk = sys.stdin.readline().strip()
+                if junk == 'quit':
+                    print("Closing server.")
+                    running = False
+            else:
+                #handle other sockets
+                data = s.recv(size)
+                if data:
+                    #do stuff
+                    s.send(data)
+                else:
+                    #this socket closed
+                    print("Connection {} closed remotely.".format(s.getpeername()))
+                    s.close()
+                    inputs.remove(s)
+
+    server.close()
+
+
+if __name__ == '__main__':
+    main()
