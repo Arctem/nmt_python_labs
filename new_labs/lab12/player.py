fe5e1b9f6ea53370a245275d57416d9a3cd85914
Russell White <rarctem@gmail.com>
2014-11-18 13:58:26 -0700

    Initial start of description of project.

--- new_labs/lab12/player.py
+++ new_labs/lab12/player.py
@@ -3,7 +3,10 @@ class Player:
     self.socket = socket
     self.name = name
 
+  #Returns False to indicate the connection should be terminated,
+  #otherwise returns True.
   def handle_message(self, msg):
     if not msg:
       return False
+    #Add custom handling code here.
     return True
