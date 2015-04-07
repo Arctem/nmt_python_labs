cf17463acf60d3e8b46f5a88519c64202245ea51
Russell White <rarctem@gmail.com>
2015-04-07 00:51:19 -0500

    Add library.py exercise.

---
+++ labs/lab10/library_test.py
@@ -0,0 +1,19 @@
+from library import Library
+
+def main():
+    lib = Library()
+    lib.add_book(Book('The Life and Lies of Albus Dumbledore', 'Rita Skeeter'))
+
+    with open('library.txt', 'r') as lib_data:
+        for line in lib_data:
+            lib.add_book(line)
+
+    print(lib)
+    print(lib.get_authors())
+    assert len(set(lib.get_authors())) == len(lib.get_authors())
+    print(lib.get_books_per_author())
+    assert len(lib.get_books_per_author()) == len(lib.get_authors())
+    
+
+if __name__ == '__main__':
+    main()
