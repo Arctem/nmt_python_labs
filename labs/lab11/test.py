493cb2b1cf4f64dc4919a7b44e7d9b2e0284e082
Russell White <rarctem@gmail.com>
2015-04-14 20:44:14 -0600

    Update test.py for Lab 11.

--- labs/lab11/test.py
+++ labs/lab11/test.py
@@ -18,14 +18,21 @@ max_speed = 1000
 accel = 0.1
 
 #hare
-#traction = 100
-#circum = 2*math.pi*0.1
-#max_speed = 500
-#accel = 1
+traction = 100
+circum = 2*math.pi*0.1
+max_speed = 500
+accel = 1
+
+#detailed
+traction = 10
+circum = 2
+max_speed = 100
+accel = 0.5
 
 for i in range(10):
+    total += actual
     speed = speed + accel * (max_speed - speed)
     ideal = speed * circum
     actual = newspeed(actual, ideal, traction)
-    total += actual
-    print('{:.2f}'.format(total))
+    #print('{:.2f}'.format(total))
+    print('{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}'.format(total, actual, ideal, speed))
