4acf8f355666e2f8f014d2f0de2a4c08434305b7
Russell White <rarctem@gmail.com>
2015-04-21 16:41:04 -0600

    Fix typo.

--- labs/project/sample_tank.py
+++ labs/project/sample_tank.py
@@ -6,7 +6,7 @@ class SampleTank(Tank):
         Tank.__init__(self)
         self.sensors = [Sensor(0, 30, 100, False), Sensor(0, 10, 50, True)]
 
-    def ai(self, delta):)
+    def ai(self, delta):
         self.set_turret_target(90)
         if self.turret_ready() and self.read_sensor(1):
            self.fire(True)
