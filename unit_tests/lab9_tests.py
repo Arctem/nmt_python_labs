84e929c9e633299f381ae2fd446ae04a0c8f97df
Hucal <hugo.rivera2319@gmail.com>
2015-10-27 17:14:33 -0600

    More tests

--- unit_tests/lab9_tests.py
+++ unit_tests/lab9_tests.py
@@ -2,7 +2,11 @@ import unittest
 import luhns
 
 class TestLab9(unittest.TestCase):
-    tests = [{i: "38520000023237", o: True}, ]
+    tests = [{i: "38520000023237", o: True},
+             {i: "49927398716", o: True},
+             {i: "49927398717", o: False},
+             {i: "1234567812345670", o: False},
+             {i: "1234567812345678", o: True}]
 
     def test_ex1(self):
         # run subtests, one for each test input
